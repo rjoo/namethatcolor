1f5870c46708521e1453af122506ef2b14508853
rjoo <ross.joo@gmail.com>
2016-02-23 17:42:20 -0500

    parse input

---
+++ README.md
@@ -0,0 +1,17 @@
+# Name That Color #
+A simple command line tool that prints the name of a hex-based color.
+
+Based off of the [name that color][http://chir.ag/projects/name-that-color/] tool by Chjirag Mehta.
+
+# Installation #
+`npm install -g namethatcolor`
+
+# Usage #
+`ntc <color>`
+
+````
+ntc "#ff9966"
+#ff9966 is orange with the exact color name: Atomic Tangerine
+ntc "#1a0dab"
+#1a0dab is blue with the closest color name: Ultramarine
+````
--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+#!/usr/bin/env node
+
 var ntc = require('./src/ntc');
 var program = require('commander');
 
+program
+  .arguments('<color>')
+  .action(function(color) {
+    if (color.substr(0) == '#') {
+      color = color.substr(1, color.length);
+    }
+
+    // match = [ matchedhex, name, matchedshaderhex, shadername, exactmatch ]
+    var match = ntc.name(color);
+    var name = match[1];
+    var shaderName = match[3].toLowerCase();
+    var isExact = match[4];
+    var resp = color + ' is ' + shaderName;
+
+    if (name.indexOf('Invalid Color') !== -1) {
+      console.log('Invalid color');
+      return;
+    }
+
+    if (isExact)
+      resp += ' with the exact color name: ';
+    else 
+      resp += ' with the closest color name: ';
+
+    resp += name;
+
+    console.log(resp);
+  })
+  .parse(process.argv);
